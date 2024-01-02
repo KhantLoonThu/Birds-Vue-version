aaacdf47ef52658affb47672bae1f425dd4eccb9
FairyLychee <fairylychee312>
2024-01-02 13:00:09 +0630

    Files added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
---
+++ src/views/BirdsDetail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "BirdsDetail",
+};
+</script>
+
+<style scoped></style>
